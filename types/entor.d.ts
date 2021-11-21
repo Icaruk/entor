40571f2b5179dd2110e378b45b4de087e18355c1
Icaruk <adri-rk@hotmail.com>
2021-11-21 17:25:46 +0100

    types

--- types/entor.d.ts
+++ types/entor.d.ts
@@ -1,50 +1,63 @@
 export = entor;
 /**
  * @typedef {Object} props
- * @property {string} envType Environment type, it prepends the files like `<envType>.entor.json`. Example: `envType = "local"` will search the file `local.entor.json` on the defined `path`.
- * @property {string} path The `<envType>.entor.json` folder path from the root. Default `./`.
- * @property {string} sharedEnvFilePath The path to the shared environment file from the root. Default `undefined`.
- * @property {function} getEnvType Function that returns the `<envType>`.
- * @property {object} override JSON that will be merged with the `<envType>.entor.json` content.
- * @property {"none" | "message" | "throw"} warningLevel
+ * @property {string} env Environment type, it prepends the files like `entor.<env>.json`. Example: `env = "local"` Entor will search the file `entor.local.json` on the defined `path`.
+ * @property {string} path Default `./`. The `entor.<env>.json` folder path from the root.
+ * @property {string} sharedEnvPath Default `undefined`. The **absolute path** to the shared environment folder path from the root. With `env = "local"` Entor will search for `local.shared.entor.json`.
+ * @property {function} getEnv Function that returns the `<env>`.
+ * @property {object} override JSON that will be merged with the `entor.<env>.json` content.
+ * @property {"none" | "message" | "throw"} warningLevel Default `message`
  * 	Handles warnings behaviour:
  * 	- `"none"`: Ignores all warnings.
  *  - `"message"`: Prints a warning message.
  *  - `"throw"`: Throws an error.
+ * @property {boolean} addToProcessEnv Default `true`. If `true` adds the `entor.<env>.json` content to the `process.env` object.
+ * @property {boolean} syncExamples Default `false`. If `true` syncs the `entor.<env>.json` file with the `entorExample.<env>.json` file.
+ *
 */
 /**
+ * Run your node app with `--env=prod` to set the `env` on startup.
  * @param {props} props
 */
-declare function entor({ envType, getEnvType, path, sharedEnvFilePath, override, addToProcessEnv, warningLevel, }: props): any;
+declare function entor({ env, getEnv, path, sharedEnvPath, override, addToProcessEnv, warningLevel, syncExamples, }: props): any;
 declare namespace entor {
     export { props };
 }
 type props = {
     /**
-     * Environment type, it prepends the files like `<envType>.entor.json`. Example: `envType = "local"` will search the file `local.entor.json` on the defined `path`.
+     * Environment type, it prepends the files like `entor.<env>.json`. Example: `env = "local"` Entor will search the file `entor.local.json` on the defined `path`.
      */
-    envType: string;
+    env: string;
     /**
-     * The `<envType>.entor.json` folder path from the root. Default `./`.
+     * Default `./`. The `entor.<env>.json` folder path from the root.
      */
     path: string;
     /**
-     * The path to the shared environment file from the root. Default `undefined`.
+     * Default `undefined`. The **absolute path** to the shared environment folder path from the root. With `env = "local"` Entor will search for `local.shared.entor.json`.
      */
-    sharedEnvFilePath: string;
+    sharedEnvPath: string;
     /**
-     * Function that returns the `<envType>`.
+     * Function that returns the `<env>`.
      */
-    getEnvType: Function;
+    getEnv: Function;
     /**
-     * JSON that will be merged with the `<envType>.entor.json` content.
+     * JSON that will be merged with the `entor.<env>.json` content.
      */
     override: object;
     /**
-     * 	Handles warnings behaviour:
-     * 	- `"none"`: Ignores all warnings.
-     *  - `"message"`: Prints a warning message.
-     *  - `"throw"`: Throws an error.
+     * Default `message`
+     * Handles warnings behaviour:
+     * - `"none"`: Ignores all warnings.
+     * - `"message"`: Prints a warning message.
+     * - `"throw"`: Throws an error.
      */
     warningLevel: "none" | "message" | "throw";
+    /**
+     * Default `true`. If `true` adds the `entor.<env>.json` content to the `process.env` object.
+     */
+    addToProcessEnv: boolean;
+    /**
+     * Default `false`. If `true` syncs the `entor.<env>.json` file with the `entorExample.<env>.json` file.
+     */
+    syncExamples: boolean;
 };
